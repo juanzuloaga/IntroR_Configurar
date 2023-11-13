1daff3a5f92c8dcc9542003819f557a3fb1aa48e
juanzuloaga <jzuloaga65@gmail.com>
2023-11-13 10:38:01 -0700

    Initial files

---
+++ to_run_configurar.R
@@ -0,0 +1,7 @@
+
+setwd("C:/Talleres_Ecodiversa_R/")
+rmarkdown::render(input = "./IntroR_Configurar/IntroR_configurar.Rmd",
+                  output_file =  "IntroR_Configurar.html",
+                  output_dir = "./IntroR_Configurar")
+
+
