e3f14916a6283809c480607da03ad798c1a33a9e
juanzuloaga <jzuloaga65@gmail.com>
2023-11-13 13:37:27 -0700

    Revisado Juan

--- to_run_configurar.R
+++ to_run_configurar.R
@@ -1,5 +1,5 @@
 
-setwd("C:/Talleres_Ecodiversa_R/")
+setwd("C:/Talleres_R/")
 rmarkdown::render(input = "./IntroR_Configurar/IntroR_configurar.Rmd",
                   output_file =  "IntroR_Configurar.html",
                   output_dir = "./IntroR_Configurar")
